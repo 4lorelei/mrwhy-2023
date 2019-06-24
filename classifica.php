514c6ae0553788f7c8bc92930f102fd3e8147501
4lorelei <43756481+4lorelei@users.noreply.github.com>
2019-06-24 13:45:13 +0200

    Add files via upload

--- classifica.php
+++ classifica.php
@@ -58,6 +58,7 @@ function leggi_classifica()
 <title>Mr. Why</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="ICON" href="gufo_.ico">
 <style type="text/css">
 
 div.titolo {
