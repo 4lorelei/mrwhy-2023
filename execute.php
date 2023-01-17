30dae474446c833e60bf2f58e69ec98c57ba77c9
4lorelei <43756481+4lorelei@users.noreply.github.com>
2023-01-17 12:45:46 +0100

    Add files via upload

--- execute.php
+++ execute.php
@@ -591,8 +591,8 @@ function keyboard_admin_menu($chatId, $msg)
 	global $botUrlMessage;
 	global $key_admin_registra, $key_admin_team, $key_admin_gara, $key_admin_set;
 	
-	$reply_markup='{"keyboard":[["'.$key_admin_set.'","'.$key_admin_team.'"],["'.$key_admin_gara.'"]],"resize_keyboard":true}';
-
+//	$reply_markup='{"keyboard":[["'.$key_admin_set.'","'.$key_admin_team.'"],["'.$key_admin_gara.'"]],"resize_keyboard":true}';
+	$reply_markup='{"keyboard":[["'."SET".'","'."TEAM".'"],["'."GARA".'"]],"resize_keyboard":true}';
 	$ch = curl_init();
 	$myUrl=$botUrlMessage . "?chat_id=" . $chatId . "&text=" . urlencode($msg). "&reply_markup=" . $reply_markup;
 	curl_setopt($ch, CURLOPT_URL, $myUrl); 
